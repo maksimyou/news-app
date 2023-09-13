e21519d99f2cb6d09ce59ffcf75796bac63e9e75
Maksim Ashmarin <93248333+maksimyou@users.noreply.github.com>
2023-09-13 11:33:07 +0300

    Delete news-app directory

--- news-app/src/components/PageUndefined/PageUndefined.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './PageUndefined.css'
-const PageUndefined = () => {
-  return (
-    <div className='page-undefined'>404 not Found</div>
-  )
-}
-export default  PageUndefined;
